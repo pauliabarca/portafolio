9afed5751e14beca3bb53e02fcdbbadfac5cb598
Pauli Abarca <paulina@abarca.cl>
2021-01-10 12:11:15 -0300

    Mi primer commit

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+});
+
+$('#enviarCorreo').click(function(){
+alert("El correo fue enviado correctamente...")
+});
+
+$("#ingredientes").on("dblclick", function(){
+$(this).css("color","red")
+});
+
+$("#preparacion").on("dblclick", function(){
+$(this).css("color","red")
+});
+
+$('.card-title').click(function(){
+$('.card-text').toggle('hide')
+});
+
+
+  $("a").click(function(event) {
+    if (this.hash !== ""){
+      event.preventDefault();
+  
+      var gato = this.hash;
+  
+      $("html, body").animate({
+        scrollTop: $(gato).offset().top
+      }, 800, function(){
+        window.location.hash = gato
+      });
+    }
+  });
