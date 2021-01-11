e6712f0294c92a63dccb1bce851649bb9c7a552b
Pauli Abarca <paulina@abarca.cl>
2021-01-10 23:18:36 -0300

    Comentarios y beautify

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,33 +2,33 @@ $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 });
 
-$('#enviarCorreo').click(function(){
-alert("El correo fue enviado correctamente...")
+$('#enviarCorreo').click(function () {
+  alert("El correo fue enviado correctamente...")
 });
 
-$("#ingredientes").on("dblclick", function(){
-$(this).css("color","red")
+$("#ingredientes").on("dblclick", function () {
+  $(this).css("color", "red")
 });
 
-$("#preparacion").on("dblclick", function(){
-$(this).css("color","red")
+$("#preparacion").on("dblclick", function () {
+  $(this).css("color", "red")
 });
 
-$('.card-title').click(function(){
-$('.card-text').toggle('hide')
+$('.card-title').click(function () {
+  $('.card-text').toggle('hide')
 });
 
 
-  $("a").click(function(event) {
-    if (this.hash !== ""){
-      event.preventDefault();
-  
-      var gato = this.hash;
-  
-      $("html, body").animate({
-        scrollTop: $(gato).offset().top
-      }, 800, function(){
-        window.location.hash = gato
-      });
-    }
-  });
+$("a").click(function (event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+
+    var gato = this.hash;
+
+    $("html, body").animate({
+      scrollTop: $(gato).offset().top
+    }, 800, function () {
+      window.location.hash = gato
+    });
+  }
+});
